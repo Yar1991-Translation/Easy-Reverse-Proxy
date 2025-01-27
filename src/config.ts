cbb8ae3969c34ad92c50eaea1800e61c392c251a
Yatmt <79678510+Yar1991-Translation@users.noreply.github.com>
2025-01-27 12:20:57 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,15 @@
 import type {Proxy} from './util/model';
+import Github from './template/github';
 
 export let PORT = 377; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXYS: Proxy[] = [];
+export let PROXYS: Proxy[] = [
+    {
+        domain: "easy-reverse-proxy-seven.vercel.app",
+        url: "https://github.com/",
+        enable: true,
+        template: Github
+    }
+];
